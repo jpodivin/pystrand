4cc6731e5133f83b25a1e1e5282f3de457ece2fa
jpodivin <jpodivin@users.noreply.github.com>
2020-12-17 16:11:06 +0000

    Tests for elitism selection.

--- tests/test_selection.py
+++ tests/test_selection.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from pystrand import Selection, Population, RandomSelection, RouletteSelection, Genotype
+from pystrand import Selection, Population, RandomSelection, RouletteSelection, ElitismSelection, Genotype
 
 class Dummy_Selection_Test(unittest.TestCase):
     test_population = Population(100, (100, 1))
@@ -85,6 +85,65 @@ class Roulette_Selection_Test(unittest.TestCase):
                 self.test_population.replace_individuals(evaluated_individuals)
 
                 selection = RouletteSelection(population_fraction)
+
+                selected_population = selection.__select__(self.test_population)
+
+                self.assertNotEqual(
+                    selected_population.population_size,
+                    self.test_population.population_size
+                    )
+
+                self.assertEqual(
+                    selected_population.max_fitness,
+                    max_fitness
+                    )
+
+                self.assertLessEqual(
+                    selected_population.population_size/self.test_population.population_size,
+                    population_fraction
+                    )
+
+class Elitism_Selection_Test(unittest.TestCase):
+
+    test_population = Population(1000, (100, 1))
+    population_fractions = [0.1, 0.5, 0.9]
+    max_fitness_values = [0.1, 0.5, 0.8]
+
+    def test_selection_init(self):
+        selection = ElitismSelection(0.5)
+        self.assertIsInstance(selection, ElitismSelection)
+
+    def test_selection_unevaluated(self):
+        for population_fraction in self.population_fractions:
+            selection = ElitismSelection(population_fraction)
+            selected_population = selection.__select__(self.test_population)
+            
+            self.assertNotEqual(
+                selected_population.population_size,
+                self.test_population.population_size
+                )
+
+            self.assertAlmostEqual(
+                selected_population.population_size/self.test_population.population_size,
+                population_fraction,
+                places=1
+                )
+
+    def test_selection_evaluated(self):
+        
+        for max_fitness in self.max_fitness_values:
+            
+            for population_fraction in self.population_fractions:
+
+                self.test_population = Population(1000, (100, 1))
+                
+                evaluated_individuals = self.test_population.individuals 
+
+                evaluated_individuals[:evaluated_individuals.shape[0]//5]['fitness'] = max_fitness
+
+                self.test_population.replace_individuals(evaluated_individuals)
+
+                selection = ElitismSelection(population_fraction)
 
                 selected_population = selection.__select__(self.test_population)
 
