e06087ec1a7ccbc563baa676c5d7167ea6174d9b
Jiri Podivin <jpodivin@gmail.com>
2021-04-10 18:01:03 +0200

    Tests for CsvLogger
    Signed-off-by: Jiri Podivin <jpodivin@gmail.com>

--- tests/test_loggers.py
+++ tests/test_loggers.py
@@ -1,5 +1,4 @@
 from unittest import TestCase, mock
-import pandas
 
 from pystrand import loggers
 
@@ -7,3 +6,29 @@ class TestCsvLogger(TestCase):
 
     def setUp(self):
         super(TestCsvLogger, self).setUp()
+
+    @mock.patch(
+        'pystrand.loggers.os.path.abspath',
+        return_value='foo/foo/bar')
+    def test_logger_init(self, mock_abspath):
+
+        logger = loggers.CsvLogger(
+            'foo/bar',
+            'fizz')
+
+        self.assertEqual('foo/foo/bar', logger.log_path)
+        self.assertEqual('fizz', logger.log_file_name)
+
+    @mock.patch(
+        'pystrand.loggers.pd.DataFrame')
+    def test_save_history_success(self, mock_dataframe):
+
+        logger = loggers.CsvLogger(
+            'foo/bar',
+            'fizz')
+
+        data = {'foo': 'bar'}
+
+        logger.save_history(data)
+
+        mock_dataframe.assert_called_once_with(data=data)
