6bb1ac6675961d1e68ea4c8e27c9bdfffb8a49e5
Jiří Podivín <66251151+jpodivin@users.noreply.github.com>
2021-04-11 14:51:23 +0200

    Permutation (#37)
    * New mutation op. based around permutations
    
    Signed-off-by: Jiri Podivin <jpodivin@gmail.com>
    
    * Revised tests to spare load.
    
    Signed-off-by: Jiri Podivin <jpodivin@gmail.com>

--- tests/test_mutations.py
+++ tests/test_mutations.py
@@ -1,62 +1,98 @@
 import unittest
 import numpy as np
 from pystrand.genotypes import Genotype
-from pystrand.mutations import PointMutation, BlockMutation
+import pystrand.mutations as mut
+
+test_genotypes = {}
+test_shapes = [
+    (i, j, k)
+        for i in range(1, 10)
+        for j in range(1, 10)
+        for k in range(1, 10)]
+
+test_gene_vals = [np.ceil(np.random.normal(scale = 10, size = 10)) for i in range(50)]
+
+for shape in test_shapes:
+    for gene_vals in test_gene_vals:
+        test_genotypes[id(shape)*id(gene_vals)] = {
+            'genotype': Genotype(
+                shape,
+                random_init=True,
+                gene_vals=gene_vals),
+            'gene_vals': gene_vals,
+            'shape': shape
+        }
 
 
 class TestPointMutation(unittest.TestCase):
 
     def setUp(self):
-        self.test_shapes = [(i, j, k) 
-                  for i in range(1, 10) 
-                  for j in range(1, 10) 
-                  for k in range(1, 10)]
-
-        self.test_gene_vals = [np.ceil(np.random.normal(scale = 10, size = 10)) for i in range(100)]
+        self.test_genotypes = test_genotypes
+        super(TestPointMutation, self).setUp()
     
     def test_genotype_mutation_bounds(self):
         """
         Checks operation of mutation operator.
         """
-        for shape in self.test_shapes:
-            for gene_vals in self.test_gene_vals:
-                genome = Genotype(shape, 
-                                random_init = True,
-                                gene_vals = gene_vals)
+        for genome in self.test_genotypes:
+            genome = self.test_genotypes[genome]
+            altered_genome = genome['genotype'].clone()
 
-                original_genome = genome.clone()
+            altered_genome.mutate(mut.PointMutation(1.0))
 
-                genome.mutate(PointMutation(1.0))
+            self.assertFalse(np.array_equiv(genome, altered_genome))
 
-                self.assertFalse(np.array_equiv(genome, original_genome))
+            self.assertTrue(
+                genome['genotype'].max() <= genome['gene_vals'].max())
 
-                self.assertTrue(genome.max() <= gene_vals.max())
-
-                self.assertTrue(genome.min() >= gene_vals.min())
+            self.assertTrue(
+                genome['genotype'].min() >= genome['gene_vals'].min())
 
 
 class TestBlockMutation(unittest.TestCase):
 
     def setUp(self):
-        self.test_shapes = [(i, j, k) 
-                  for i in range(1, 10) 
-                  for j in range(1, 10) 
-                  for k in range(1, 10)]
-
-        self.test_gene_vals = [np.ceil(np.random.normal(scale = 10, size = 10)) for i in range(100)]
+        self.test_genotypes = test_genotypes
+        super(TestBlockMutation, self).setUp()
     
     def test_genotype_mutation_bounds(self):
         """
         Checks operation of mutation operator.
         """
-        for shape in self.test_shapes:
-            for gene_vals in self.test_gene_vals:
-                genome = Genotype(shape, 
-                                random_init = True,
-                                gene_vals = gene_vals)
+        for genome in self.test_genotypes:
+            genome = self.test_genotypes[genome]
+            altered_genome = genome['genotype'].clone()
 
-                genome.mutate(BlockMutation(1.0, block_size=10))
+            altered_genome.mutate(mut.BlockMutation(1.0))
 
-                self.assertTrue(genome.max() <= gene_vals.max())
+            self.assertFalse(np.array_equiv(genome, altered_genome))
 
-                self.assertTrue(genome.min() >= gene_vals.min())
+            self.assertTrue(
+                genome['genotype'].max() <= genome['gene_vals'].max())
+
+            self.assertTrue(
+                genome['genotype'].min() >= genome['gene_vals'].min())
+
+class TestPermutationMutation(unittest.TestCase):
+
+    def setUp(self):
+        self.test_genotypes = test_genotypes
+        super(TestPermutationMutation, self).setUp()
+    
+    def test_genotype_mutation_bounds(self):
+        """
+        Checks operation of mutation operator.
+        """
+        for genome in self.test_genotypes:
+            genome = self.test_genotypes[genome]
+            altered_genome = genome['genotype'].clone()
+
+            altered_genome.mutate(mut.PermutationMutation(1.0))
+
+            self.assertFalse(np.array_equiv(genome, altered_genome))
+
+            self.assertTrue(
+                genome['genotype'].max() <= genome['gene_vals'].max())
+
+            self.assertTrue(
+                genome['genotype'].min() >= genome['gene_vals'].min())
