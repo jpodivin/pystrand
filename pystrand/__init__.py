29b9d1e86a9029d5c8005457b8286e5ea2712cb0
Jiri Podivin <jpodivin@gmail.com>
2021-04-17 19:47:08 +0200

    These don't have to explicit.
    Signed-off-by: Jiri Podivin <jpodivin@gmail.com>

--- pystrand/__init__.py
+++ pystrand/__init__.py
@@ -1,7 +1,2 @@
+"""Pystrand main module.
 """
-Import pystrand names.
-"""
-#Files
-from pystrand import genotypes, optimizers, populations, selections
-
-__all__ = ['genotypes', 'optimizers', 'populations', 'selections']
