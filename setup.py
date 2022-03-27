48a3d60b06827a724a7d7b784517979f8b6c8b42
Jiri Podivin <jpodivin@gmail.com>
2022-03-27 16:46:17 +0200

    Getting ready for packaging
    Signed-off-by: Jiri Podivin <jpodivin@gmail.com>

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering :: Artificial Life",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=[
         "numpy"
     ],
